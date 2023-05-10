9ff3bbe9cbf32d7d5b39daa72e0d63929ca58864
bagasjs <nakanoyoichi83@gmail.com>
2023-05-10 23:53:21 +0700

    Ready to dockerize

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,10 @@
 
 @section("body")
 @include("_utils.navbar")
-<section class="bg-unpad-yellow mt-3">
+<section class="bg-image-refenitiv bg-gray-500 bg-no-repeat bg-blend-multiply mt-6">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-unpad-dark md:text-5xl lg:text-6xl dark:text-white">Database Refinitiv</h1>
-        <p class="mb-8 text-lg font-normal text-unpad-dark lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Aplikasi database refinitiv merupakan aplikasi berbasis web yang bertujuan untuk melakukan booking jadwal penggunaan perpustakaan Fakultas Ekonomi Bisnis Universitas Padjajaran</p>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-unpad-light md:text-5xl lg:text-6xl dark:text-white">Database Refinitiv</h1>
+        <p class="mb-8 text-lg font-normal text-unpad-light lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Aplikasi database refinitiv merupakan aplikasi berbasis web yang bertujuan untuk melakukan booking jadwal penggunaan perpustakaan Fakultas Ekonomi Bisnis Universitas Padjajaran</p>
         @if($nearest_close_schedule != null)
         <div class="flex p-4 mx-4 my-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-400 dark:border-yellow-800" role="alert">
             <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
