271c5042eeae8c7455ff3adf1f82363884b660c8
bagasjs <nakanoyoichi83@gmail.com>
2023-08-05 02:01:55 +0700

    Added password change

--- app/Http/Controllers/Profile/ChangePasswordController.php
+++ app/Http/Controllers/Profile/ChangePasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PasswordModificationRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
 {
@@ -16,6 +17,8 @@ class ChangePasswordController extends Controller
     public function update(PasswordModificationRequest $request)
     {
         $data = $request->validated();
-        dd($data);
+        $data["password"] = Hash::make($data["password"]);
+        User::find(auth()->user()->id)->update($data);
+        return redirect(route("profile.index"))->with("success", "Password changed");
     }
 }
